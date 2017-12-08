b75ff4db430487a315c130cd9e343589c74fd645
ToniTurnerr <tonipetrov6@gmail.com>
2017-12-08 16:43:16 +0100

    removed leftover stuff from my other bot

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = client => {
-	
-	console.log('[Starting Message]: I\'m online!');
-	client.user.setPresence({ game: { name: ('Exodus Network'), type: 0 } });
-	client.user.setStatus('online');	
-}
